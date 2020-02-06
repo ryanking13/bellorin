5a7040ec303152121426f003381bb55ffa15ba80
sihadan <def6488@gmail.com>
2020-02-06 14:18:07 +0900

    add tistory full post crawling

--- tistory.py
+++ tistory.py
@@ -2,7 +2,10 @@ from datetime import datetime
 import json
 import pathlib
 import logging
+import re
+import time
 import requests
+from bs4 import BeautifulSoup
 import config
 from crawler import Crawler
 
@@ -19,9 +22,51 @@ class Tistory(Crawler):
         self._analyser = None
         self._session = requests.session()
 
-        self._session.headers.update({"Authorization": f"KakaoAK {self.app_key}"})
+        self._session.headers.update(
+            {
+                "Authorization": f"KakaoAK {self.app_key}",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
+            }
+        )
 
-    def crawl(self, query, start_date, end_date):
+    def _parse_post(self, url):
+        r = self._session.get(url=url)
+        soup = BeautifulSoup(r.text, "html.parser")
+        content = ""
+
+        # 1) content
+
+        # 티스토리 블로그에서 글 내용에 사용되는 클래스 목록
+        #! WARNING: 실험적으로 찾아낸 값으로, 상황에 따라 업데이트 필요
+        content_class = (
+            "tt_article_useless_p_margin",  # https://cow5jean.tistory.com/29
+            "article_view",  # https://milkbean.tistory.com/16?category=842530
+            "entry-content",  # https://simyeju.tistory.com/48
+            "post-content",  # https://philipbox.tistory.com/79
+            "desc",  # https://michaelchoi.tistory.com/25
+            "article_cont",  # https://iton.tistory.com/4090
+            "article",  # https://freepanda.tistory.com/47
+            "area_view",  # https://choish313.tistory.com/122
+        )
+        found = False
+        for tag in soup.find_all("div"):
+            tag_class = tag.get("class", [])
+            for ccls in content_class:
+                if ccls in tag_class:
+                    content = tag.text.strip()
+                    content = re.sub(r"\s+", " ", content)  # compress whitespaces
+                    found = True
+                    break
+            if found:
+                break
+        else:  # not found
+            self._log(f"NOT FOUND: content ({url})", False)
+
+        return {
+            "content": content,
+        }
+
+    def crawl(self, query, start_date, end_date, full=True):
         url = "https://dapi.kakao.com/v2/search/blog"
         display_size = 50
         params = {
@@ -69,10 +114,26 @@ class Tistory(Crawler):
                     "title": document["title"],
                     "blogname": document["blogname"],
                     "postUrl": document["url"],
+                    "blogUrl": document["url"][
+                        : document["url"].rindex("/")
+                    ],  # strip rightmost slash
                     "summary": document["contents"],
                     "thumbnailUrl": document["thumbnail"],
                     "created": postdate.isoformat(),
                 }
+
+                # kakao search api does not reveal full blog data,
+                # therefore, manual crawling needed to get full data.
+                # however, this might be considered as an malicious behavior.
+                if full:
+                    try:
+                        post_full = self._parse_post(document["url"])
+                        post_data.update(post_full)
+                        time.sleep(0.1)  # prevent massive request
+                    except Exception as e:
+                        self._log(f"Parsing blog failed {document['url']}", False)
+                        self._log(e)
+                        pass
 
                 posts.append(post_data)
 
